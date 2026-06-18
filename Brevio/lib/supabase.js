7e81a47141641985c5375b51621d47645bd17775
qHikazuq <8c8vnprt49@privaterelay.appleid.com>
2026-06-18 12:06:53 +0200

    Update supabase.js

--- Brevio/lib/supabase.js
+++ Brevio/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(
+  "https://arjjfcggyftgogpovqks.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFyampmY2dneWZ0Z29ncG92cWtzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyMjg4MzUsImV4cCI6MjA5MjgwNDgzNX0.AFIAtGSUzS-P4HJAMS-6Gab5rMh4NUtC82VYRipzMnE"
+);
